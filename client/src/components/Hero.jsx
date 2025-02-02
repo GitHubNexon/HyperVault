0165ae2d2e5edcc2ba7427631e53c531a86ab930
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-02-02 12:40:37 +0800

    added some shits

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import ThreeScene from "./ThreeScene";
-import Hero_BG from "../assets/videos/Hero-Bg.mp4";
-import ParticleBackground from "./ParticleBackground";
+import Carousel from "../components/Carousel.jsx";
 
 const Hero = () => {
-
-  
   return (
     <div className="relative w-full h-screen flex flex-col items-center justify-between">
       {/* 3D Scene */}
-        <ThreeScene />
+      <ThreeScene />
 
       {/* Description Section */}
-      <div className="absolute top-1/2 transform -translate-y-1/2 text-center mt-8 px-4 max-w-lg w-full z-10">       
+      <div className="absolute top-1/2 transform -translate-y-1/2 text-center mt-8 px-4 max-w-lg w-full z-10 ">
         <p className="text-[0.9rem] sm:text-xl mb-6">
-        "Hi, I'm John Mark Lilio Pulmano — a passionate programmer and tech enthusiast dedicated to crafting innovative solutions. 
+          "Hi, I'm John Mark Lilio Pulmano — a passionate programmer and tech
+          enthusiast dedicated to crafting innovative solutions.
         </p>
         <div className="flex justify-center gap-4">
           <button className="bg-blue-500 px-6 py-3 rounded-lg text-lg  transition duration-300">
@@ -25,6 +23,9 @@ const Hero = () => {
           </button>
         </div>
       </div>
+      {/* <div className="mt-10">
+        <Carousel />
+      </div> */}
     </div>
   );
 };
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Hero from "../components/Hero.jsx";
+import Carousel from "../components/Carousel.jsx";
 
 const Home = () => {
   return (
     <>
       <div id="home">
         <Hero />
+        {/* <Carousel /> */}
       </div>
     </>
   );
